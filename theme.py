e450b2e23a9f649d849f86f5b1f11548a4a81c09
zfl9 <j1002238565@gmail.com>
2017-05-08 18:14:50 +0800

    update theme.py

--- theme.py
+++ theme.py
@@ -8,50 +8,73 @@
 __author__ = 'Otokaze 738158186@qq.com'
 
 def jdlingyu():
-    import requests,logging,os,re,html
+    import requests,logging,os,re,html,argparse,sys
 
+    # logger
     logger=logging.getLogger(os.path.abspath(__file__))
     logger.setLevel(logging.ERROR)
-    handler=logging.FileHandler('jdlingyu_theme.log')
+    handler=logging.FileHandler('{}.log'.format(os.path.splitext(__file__)[0]))
     handler.setLevel(logging.ERROR)
     formatter=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
+    # argparse
+    parser=argparse.ArgumentParser(description='parse pictures from http://www.jdlingyu.moe')
+    parser.add_argument('url', help='To download the URL path')
+    parser.add_argument('-o', '--output_path', help='Specify file save path', default=os.path.join(os.path.abspath('.'), 'download'))
+    parser.add_argument('-q', '--quiet', help='Do not display output information', action='store_true')
+    args=parser.parse_args()
+
+    # 参数检查 - url
+    if type(args.url) != type(None):
+        url_pre=args.url.strip().lower()
+        re_chk_url=re.compile(r'^http(?:s)?://[^\s]+$')
+        chk_url=re.search(re_chk_url,url_pre)
+        if chk_url != None:
+            url=url_pre
+        else:
+            print('Input is not standard URL!')
+            sys.exit(1)
+    else:
+        print('You don\'t enter anything!')
+        sys.exit(1)
+
+    # 参数检查 - path
+    path_pre=args.output_path.strip()
     current_os=os.name
     if current_os == 'posix':
-        path_format=r'/root/jdlingyu'
+        re_chk_path=re.compile(r'^\/.*')
+        re_chk_path_current=re.compile(r'^[^\/].*')
+        chk_path=re.search(re_chk_path,path_pre)
+        chk_path_current=re.search(re_chk_path_current,path_pre)
+        if chk_path != None:
+            path=path_pre
+            if not os.path.isdir(path):
+                os.mkdir(path)
+        elif chk_path_current !=  None:
+            path=os.path.join(os.path.abspath('.'), path_pre)
+            if not os.path.isdir(path):
+                os.mkdir(path)
     elif current_os == 'nt':
-        path_format=r'd:/jdlingyu'
-
-    while True:
-        url=input('url(eg: http://www.jdlingyu.moe/16289/): ').strip().lower()
-        re_chk_url=re.compile(r'^http(?:s)?://[^\s]+$')
-        chk_url=re.search(re_chk_url,url)
-        if chk_url != None:
-            break
-
-    while True:
-        path=input('save_path(eg: %s): ' % path_format).strip()
-        if current_os == 'posix':
-            re_chk_path=re.compile(r'^/[^/\s].*$')
-            chk_path=re.search(re_chk_path,path)
-            if chk_path != None:
-                if not os.path.isdir(path):
-                    os.mkdir(path)
-                break
-        elif current_os == 'nt':
-            re_chk_path=re.compile(r'^[c-zC-Z]:/[^\s].*$')
-            chk_path=re.search(re_chk_path,path)
-            if chk_path != None:
-                if not os.path.isdir(path):
-                    os.mkdir(path)
-                break
+        re_chk_path=re.compile(r'^[a-zA-Z]:.*')
+        re_chk_path_current=re.compile(r'^[^a-zA-Z][^:].*')
+        chk_path=re.search(re_chk_path,path_pre)
+        chk_path_current=re.search(re_chk_path_current,path_pre)
+        if chk_path != None:
+            path=path_pre
+            if not os.path.isdir(path):
+                os.mkdir(path)
+        elif chk_path_current != None:
+            path=os.path.join(os.path.abspath('.'), path_pre)
+            if not os.path.isdir(path):
+                os.mkdir(path)
 
     headers={
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36',
     'Connection':'close'
     }
+
     try:
         r=requests.get(url,headers=headers)
         r.encoding='utf-8'
@@ -76,7 +99,7 @@ def jdlingyu():
         'http://www.jdlingyu.moe/wp-content/uploads/2017/01/2017-01-07_20-57-14.png',
         'http://www.jdlingyu.moe/wp-content/uploads/2017/01/2017-01-07_00-26-46.png'
         }
-    
+
         title=html.unescape(re.search(re_title,data_html).group(1))
         path_title=os.path.join(path,title)
         if not os.path.isdir(path_title):
@@ -96,9 +119,9 @@ def jdlingyu():
                     for chunk in r_pic.iter_content(1024):
                         f.write(chunk)
                     r_pic.close()
-            except BaseException as e:
+            except Exception as e:
                 logger.error(e)
-    except BaseException as e:
+    except Exception as e:
         logger.error(e)
 
 if __name__ == '__main__':
