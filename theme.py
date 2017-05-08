15ea3cc71f28073ef2e982fb15f73d5ca24a3d22
zfl9 <j1002238565@gmail.com>
2017-05-08 19:27:40 +0800

    theme.py is complete! version 2.0

--- theme.py
+++ theme.py
@@ -10,20 +10,13 @@ __author__ = 'Otokaze 738158186@qq.com'
 def jdlingyu():
     import requests,logging,os,re,html,argparse,sys
 
-    # logger
-    logger=logging.getLogger(os.path.abspath(__file__))
-    logger.setLevel(logging.ERROR)
-    handler=logging.FileHandler('{}.log'.format(os.path.splitext(__file__)[0]))
-    handler.setLevel(logging.ERROR)
-    formatter=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
     # argparse
     parser=argparse.ArgumentParser(description='parse pictures from http://www.jdlingyu.moe')
     parser.add_argument('url', help='To download the URL path')
     parser.add_argument('-o', '--output_path', help='Specify file save path', default=os.path.join(os.path.abspath('.'), 'download'))
+    parser.add_argument('-l', '--log', help='save log to file', action='store_true')
     parser.add_argument('-q', '--quiet', help='Do not display output information', action='store_true')
+    parser.add_argument('-v', '--version', help='Display program version', action='version', version='jdlingyu.py v2.0')
     args=parser.parse_args()
 
     # 参数检查 - url
@@ -70,49 +63,75 @@ def jdlingyu():
             if not os.path.isdir(path):
                 os.mkdir(path)
 
+    # 参数检查 - log
+    if args.log:
+        logger=logging.getLogger(os.path.abspath(__file__))
+        logger.setLevel(logging.ERROR)
+        handler=logging.FileHandler('{}.log'.format(os.path.splitext(__file__)[0]))
+        handler.setLevel(logging.ERROR)
+        formatter=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
     headers={
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36',
     'Connection':'close'
     }
 
     try:
+        # 排除无关图片
+        ## 样本1
+        r_exclude_1=requests.get('http://www.jdlingyu.moe/28721/', headers=headers)
+        r_exclude_1.encoding='utf-8'
+        data_html_exclude_1=r_exclude_1.text
+        r_exclude_1.close()
+        re_exclude_1_all_pic1=re.compile(r'http://www.jdlingyu.moe/wp-content/uploads/\d{4}/\d{2}/\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}(?:-\d+?)?\.(?:jpg|jpeg|png|gif)')
+        re_exclude_1_all_pic2=re.compile(r'http://ww\d.sinaimg.cn/large/(.*?\.(?:jpg|jpeg|png|gif))')
+        re_exclude_1_all_pic=re.findall(re_exclude_1_all_pic1, data_html_exclude_1) + re.findall(re_exclude_1_all_pic2, data_html_exclude_1)
+        ## 样本2
+        r_exclude_2=requests.get('http://www.jdlingyu.moe/16289/', headers=headers)
+        r_exclude_2.encoding='utf-8'
+        data_html_exclude_2=r_exclude_2.text
+        r_exclude_2.close()
+        re_exclude_2_all_pic1=re.compile(r'http://www.jdlingyu.moe/wp-content/uploads/\d{4}/\d{2}/\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}(?:-\d+?)?\.(?:jpg|jpeg|png|gif)')
+        re_exclude_2_all_pic2=re.compile(r'http://ww\d.sinaimg.cn/large/(.*?\.(?:jpg|jpeg|png|gif))')
+        re_exclude_2_all_pic=re.findall(re_exclude_2_all_pic1, data_html_exclude_2) + re.findall(re_exclude_2_all_pic2, data_html_exclude_2)
+        ## 获取无关图片url
+        exclude_1=set(re_exclude_1_all_pic)
+        exclude_2=set(re_exclude_2_all_pic)
+        ex_pic=exclude_1 & exclude_2
+
+        # requests_get
         r=requests.get(url,headers=headers)
         r.encoding='utf-8'
         data_html=r.text
         r.close()
+
+        # regexp预编译
         re_title=re.compile(r'<title>(.*?)</title>')
         re_pic1=re.compile(r'(http://www.jdlingyu.moe/wp-content/uploads/\d{4}/\d{2}/(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}(?:-\d+?)?\.(?:jpg|jpeg|png|gif)))')
         re_pic2=re.compile(r'(http://ww\d.sinaimg.cn/large/(.*?\.(?:jpg|jpeg|png|gif)))')
-        ex_pic={
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/02/2016-02-04_17-40-21.png',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/09/2016-09-17_16-28-29.jpg',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/10/2016-10-14_13-50-19.jpg',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/11/2016-11-04_16-16-09.jpg',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/03/2016-03-07_16-37-20.png',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/07/2016-07-23_16-12-13.jpg',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/09/2016-09-17_16-31-37.jpg',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/07/2016-07-23_15-53-05.jpg',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/10/2016-10-30_00-47-09.png',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/10/2016-10-03_22-10-06.jpg',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/10/2016-10-03_22-15-50.jpg',
-        'http://www.jdlingyu.moe/wp-content/uploads/2016/08/2016-08-22_11-15-19.jpg',
-        'http://www.jdlingyu.moe/wp-content/uploads/2017/01/2017-01-07_20-57-14.png',
-        'http://www.jdlingyu.moe/wp-content/uploads/2017/01/2017-01-07_00-26-46.png'
-        }
 
+        # 获取title
         title=html.unescape(re.search(re_title,data_html).group(1))
         path_title=os.path.join(path,title)
         if not os.path.isdir(path_title):
             os.mkdir(path_title)
+
+        # 获取picture_urls
         pic1=re.findall(re_pic1,data_html)
         pic2=re.findall(re_pic2,data_html)
-        pic=pic1+pic2
-        print('\n------------------------ %s -----------------------' % url)
+        pic=set(pic1) | set(pic2)
+
+        # 保存图片
+        if not args.quiet:
+            print('\n------------------------ %s -----------------------' % url)
         for picurl,picname in pic:
             if picurl in ex_pic:
                 continue
             path_pic=os.path.join(path_title,picname)
-            print('url: %s' % picurl)
+            if not args.quiet:
+                print('url: %s' % picurl)
             try:
                 with open(path_pic,'wb') as f:
                     r_pic=requests.get(picurl,headers=headers)
